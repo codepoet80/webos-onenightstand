2eb243244747cc289f3ea4973af27b4acbd2042a
CodePoet80 <jonathan.m.wise@gmail.com>
2020-12-04 20:07:12 -0500

    stub in updater

---
+++ app/models/updater-model.js
@@ -0,0 +1,19 @@
+/*
+Updater Model
+ Version 0.1
+ Created: 2020
+ Author: Jonathan Wise
+ License: MIT
+ Description: A model to check for and get updates from App Museum II web service.
+    Does not require App Museum to be installed, but does require Preware and internet access.
+*/
+
+var UpdaterModel = function() {
+
+};
+
+//Check App Museum II web service to see if there are any updates
+UpdaterModel.prototype.CheckForUpdate = function() {}
+
+//Ask PreWare
+UpdaterModel.prototype.InstallUpdate = function() {}
